fb0819f6f180d091cb4d1afc2aa7f578882111d0
Teemu Koivisto <tkoivisto456@gmail.com>
2016-11-15 19:51:34 +0200

    Create modal

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-# sup
-
-Simple bootstrap my ass. This one is really complicated x_x. That's what happens when
-you're dealing with ultra-modular and tip of the edge technology. Things change,
-sometimes rapidly. So this bootstrap might already be outdated when I created it,
-but ehh it's fine. Only thing you really need is to have one sturdy project scaffold
-that can withstand the tides of major updates and the pains of testing. So I try
-to keep this one at minimal complexity but yeah, this react stuff is a lot more
-complicated than Angular 1.0. GLHF.
-
-Don't mind about the error in console about react-router. Supposedly it will be fixed
-in the next-next major release 4.0 of react-router. :DD And fixing it now would really
-make me flip my burgers hard so I won't do it.
+# I am very bad at programming :((
--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -4,20 +4,73 @@ import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
 export default class MapThing extends React.Component {
 
-  render() {
-    const position = [60.192059, 24.945831];
+  constructor() {
+    super();
+    this.state = {
+      showModal: true
+    };
+  }
+
+  handleClick(type, event) {
+    if (type === "closeModal") {
+      this.setState({
+        showModal: false,
+      })
+    }
+  }
+
+  renderModal() {
     return (
-      <Map center={position} zoom={15}>
-        <TileLayer
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker position={position}>
-          <Popup>
-            <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
-          </Popup>
-        </Marker>
-      </Map>
+      <div id="grappaModal" className={this.state.showModal ? "grappa-modal show" : "grappa-modal"}>
+        <div className="grappa-modal-content">
+          <h2>Ping your connection</h2>
+          <h3>Select your speed:</h3>
+          <button className="btn primary" onClick={this.handleClick.bind(this, "closeModal")}>OK</button>
+        </div>
+      </div>
+    )
+  }
+
+  render() {
+    /*
+      window{
+      ping your connection data
+      result:
+        yourspeed
+        your network capacity
+        signal bad/good/excellent
+      how to improve your connection>>
+      }
+    */
+    const position = [60.192059, 24.945831];
+    // const position = [0, 0];
+    return (
+      <div>
+        { this.renderModal() }
+        <Map center={position} zoom={15}>
+          <TileLayer
+            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+            // url="https://kartat.dna.fi/Peka4Proxy/Taustakartta_Harmaasavy_v1/MapServer/tile/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={position}>
+            <Popup>
+              <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+            </Popup>
+          </Marker>
+        </Map>
+      </div>
     );
   }
 }
+
+/*
+https://kartat.dna.fi/Peka4Proxy/Taustakartta_Harmaasavy_v1/MapServer/tile/1/7/12
+https://kartat.dna.fi/Peka4Proxy/Peittokartta_GSM_EDGE_Julk_v2/MapServer/tile/0/5/10
+https://kartat.dna.fi/Peka4Proxy/Peittokartta_3G900_3G2100_Julk_v2/MapServer/tile/0/6/1
+
+https://kartat.dna.fi/Peka4Proxy/Peittokartta_LTE1800Julk_v2/MapServer/tile/0/5/11
+https://kartat.dna.fi/Peka4Proxy/Peittokartta_LTE800Julk_v2/MapServer/tile/0/6/10
+
+https://kartat.dna.fi/Peka4Proxy/Peittokartta_HDTVJulk_v2/MapServer/tile/0/6/10
+*/
